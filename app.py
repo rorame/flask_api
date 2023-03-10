583bca50398c543c4377af8ccd1f237b05e6194e
rorame <roman.menyalschikov@yandex.ru>
2023-03-10 13:59:59 +0300

    added post and get requests for postgres

--- app.py
+++ app.py
@@ -1,24 +1,31 @@
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Api, Resource, reqparse
 from flask_sqlalchemy import SQLAlchemy
-
+from flask_migrate import Migrate
 
 app = Flask(__name__)
-# TODO довести до ума конфиги для postgres
-# app.config['SQLALCHEMY_DATABASE_URI'] =
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:postgres@localhost:5432/postgres"
 
 api = Api(app)
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 
 class Quotes(db.Model):
+    __tablename__ = 'quotes'
+    # TODO сделать автозаполнение id
     id = db.Column(db.Integer, primary_key=True)
     author = db.Column(db.String(100), nullable=False)
     quote = db.Column(db.String(500), nullable=False)
 
+    def __init__(self, id, author, quote):
+        self.id = id
+        self.author = author
+        self.quote = quote
 
-# with app.app_context():
-#     db.create_all()
+    def __repr__(self):
+        return f"<Quote {self.id}>"
+
 
 quotes = [
     {
@@ -37,6 +44,42 @@ quotes = [
                  "Humans, who are limited by slow biological evolution, " +
                  "couldn't compete, and would be superseded."
     }]
+
+
+@app.route('/quotes', methods=['POST', 'GET'])
+def handle_quotes():
+    if request.method == 'POST':
+        if request.is_json:
+            data = request.get_json()
+            new_quote = Quotes(id=data['id'], author=data['author'], quote=data['quote'])
+            db.session.add(new_quote)
+            db.session.commit()
+            return {"message": f"quote {new_quote.id} has been created successfully."}
+        else:
+            return {"error": "The request payload is not in JSON format"}
+
+    # elif request.method == 'GET':
+    #     quotes = Quotes.query.all()
+    #     results = [
+    #         {
+    #             "author": quote.author,
+    #             "quote": quote.quote,
+    #         } for quote in quotes]
+    #
+    #     return {"count": len(results), "quotes": results}
+
+
+@app.route('/api', methods=['GET'])
+def quotes():
+    quotes = Quotes.query.all()
+    results = [
+        {
+            "id": quote.id,
+            "author": quote.author,
+            "quote": quote.quote,
+        } for quote in quotes]
+
+    return {"count": len(results), "quotes": results}
 
 
 class Quote(Resource):
@@ -60,7 +103,6 @@ class Quote(Resource):
     #     quote = {
     #         'id': id
     #     }
-
 
 
 class QuoteList(Resource):
